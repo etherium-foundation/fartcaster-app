ab95dbc4e8692c5a4bd0d4c16c1a6875c2ba19a5
Barabazs <31799121+Barabazs@users.noreply.github.com>
2025-07-25 11:52:24 +0000

    feat: replace icon and splash images with new ETH image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,9 +4,9 @@ export const APP_DESCRIPTION =
   "The official fartcaster app for the Etherium Foundation.";
 export const APP_PRIMARY_CATEGORY = "social";
 export const APP_TAGS = ["ith", "etherium", "foundation", "vitamin", "vitalek"];
-export const APP_ICON_URL = `${APP_URL}/icon.png`;
+export const APP_ICON_URL = `${APP_URL}/eth.png`;
 export const APP_OG_IMAGE_URL = `${APP_URL}/opengraph-image`;
-export const APP_SPLASH_URL = `${APP_URL}/splash.png`;
+export const APP_SPLASH_URL = `${APP_URL}/eth.png`;
 export const APP_SPLASH_BACKGROUND_COLOR = "#f7f7f7";
 export const APP_BUTTON_TEXT = "Open for ITH!";
 export const DEFAULT_OG_SIZE = {
