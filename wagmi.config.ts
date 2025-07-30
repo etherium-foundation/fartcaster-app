dc73d7624964c107c47445faf27acc3a6160092f
Barabazs <31799121+Barabazs@users.noreply.github.com>
2025-07-30 08:19:57 +0000

    feat: add ITHNFT ABI and integrate into wagmi configuration

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@wagmi/cli";
 import { react } from "@wagmi/cli/plugins";
 
 import { ithAbi } from "./src/abi/ithAbi";
+import { ithNftAbi } from "@/abi/ithNftAbi";
 
 export default defineConfig({
   out: "src/wagmi/generated.ts",
@@ -10,6 +11,11 @@ export default defineConfig({
       name: "ITH",
       abi: ithAbi,
       address: "0xC5471B96D3D8bB5d387C04139f4a3d0626D95330",
+    },
+    {
+      name: "ITHNFT",
+      abi: ithNftAbi,
+      address: "0x5b21eA285ca04807c31686fD6BAF9a80a46bA692", //TODO: update with mainnet address
     },
   ],
   plugins: [react()],
